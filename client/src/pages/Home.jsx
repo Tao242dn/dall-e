7d9b4263188b67685cd6041687e26b47ea87e8b4
ThuanVo242 <thuanvo0204@gmail.com>
2023-02-09 09:36:17 +0700

    Second Commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/posts', {
+      const response = await fetch('https://dall-e-0fjs.onrender.com/api/v1/posts', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -45,7 +45,7 @@ const Home = () => {
   }, []);
 
   const handleSearchChange = (e) => {
-    setSearchText(e.target.value);
+    setSearchText(searchTimeout);
 
     setSearchTimeout(
       setTimeout(() => {
